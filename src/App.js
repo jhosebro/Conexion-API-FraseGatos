a0c152337d0b7f0c8fce2d021b5ec1413a554b64
Jhoan Rojas <jhosebro2108@gmail.com>
2022-09-21 15:26:20 -0500

    Commit desde dentro de prueba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const GIPHY_API_KEY = 'wrb2vLSM6Hwl7A063rw7rkU1AhzyVXsp';
+
+  const [catFact, setCatFact] = useState("");
+  const [catGift, setCatGift] = useState("");
+  
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const callGiphyAPI = (string) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${string}&api_key=${GIPHY_API_KEY}`)
+    .then(response => response.json())
+    .then(data => setCatGift(data.data[0].images.original.url))
+    console.log(catGift); 
+  }
+
+  const callAPI = () => {
+    fetch('https://catfact.ninja/fact')
+    .then((res) => res.json())
+    .then((data) => { 
+      setCatFact(data.fact || 'Hola jhosebro');
+      callGiphyAPI(data?.fact?.split(" ",3).join(" "));
+      console.log(data.fact);
+   });
+
+  };
+
+
+  useEffect(callAPI,[]);
+    
+
+
+
+    return (
+    <div style={{display: 'flex',justifyContent: 'center', gap:'50px', alignItems: 'center'}}>
+    <img src={catGift} style={{objectFit: 'cover' ,width: '200px', height: '200px'}} alt=''/>
+    <h1>{catFact}</h1>
     </div>
-  );
+    );
 }
 
 export default App;
